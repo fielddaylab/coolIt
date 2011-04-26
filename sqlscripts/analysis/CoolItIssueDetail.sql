8f27d268d309300a41fa6f2f2fcdf5879f4524c9
clalande <clalande@d64d4213-cb0a-0410-9606-839883b54457>
2011-04-26 14:02:54 +0000

    adding sql scripts used for data analysis
    git-svn-id: https://code.doit.wisc.edu/svn/at-default/ls/CoolIt/trunk@17145 d64d4213-cb0a-0410-9606-839883b54457

---
+++ sqlscripts/analysis/CoolItIssueDetail.sql
@@ -0,0 +1,16 @@
+--review records with a specific input
+select *
+from States s1
+inner join Episodes e1 on s1.episodeId = e1.id
+where problemId = 1
+and coolerId = 3
+and materialId = 11
+and length = 0.5
+and crossSection = 0.001
+and powerFactor = 1
+order by s1.id
+
+select *
+from States
+where EpisodeId in (628)
+order by id
